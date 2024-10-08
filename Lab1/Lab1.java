f6c90fc7a407eddbfe00a7d51f958b7a78d510db
Gemilang-dev <gemilang280505@gmail.com>
2024-10-08 09:26:35 +0200

    Hotfix-main method to Lab 1 method

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,95 +1,81 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}
-
+import java.util.Scanner;
+public class Lab1 {
 // 1. Create a program that prints your name to the screen.
 
-// public class Main {
+
 //    public static void main(String[] args){
 //        System.out.println("My name is Rayi Aqli Gemilang");
 //    }
-// }
+
+
 
 // 2. Create a program that prints out the following:
 // (print hello world and new line)
-//public class Main {
-//    public static void main(String[] args){
-//        System.out.println("hello world");
-//        System.out.print(" (And all people of the world)");
-//    }
-//}
+
+        // public static void main(String[] args){
+        //     System.out.println("hello world");
+        //     System.out.print(" (And all people of the world)");
+        // }
+
 
 //3. Create a program that counts and displays how many seconds there are in
 //    a year. You can assume that a year consists of 365 days
 //    (therefore the year is not a leap year).
 
-//public class Main{
 //    public static void main(String[] args){
 //        System.out.println("In a year, there are " + (365*24*60*60) + " seconds");
 //    }
-//}
+
 
 //4.  Create a program that asks the user for two integers and then prints their sum.
 
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//
-//
+
+
 //    public static int SumOfTwo(int NumF, int NumS) {
 //     return NumF + NumS;
 //    }
-//
+
 //    public static void main(String[] args){
-//
+
 //        Scanner input = new Scanner(System.in);
-//
+
 //        System.out.print("Enter first Number");
 //        int NumF = input.nextInt();
 //        System.out.print("Enter second Number");
 //        int NumS = input.nextInt();
-//
+
 //        System.out.print("Sum of " + NumF + " and " + NumS + "= " + SumOfTwo(NumF, NumS));
-//
+
 //    }
-//}
+
 
 //5. Create a program that asks the user for a number and tells if the
 //number is positive (i.e. greater than zero).
 
-//import java.util.Scanner;
-//
-//public class Main{
 //
 //    public static void main(String[] args){
 //        Scanner input = new Scanner(System.in);
-//
+
 //        System.out.print("input the number :");
 //        int Num = input.nextInt();
 //        if (Num > 0){
 //            System.out.print(" That is positif number");
 //        }
-//        else{
+//        else {
 //            System.out.print("That not positive number");
 //        }
-//
+// //
 //    }
-//
-//}
+// //
+
 //
 //6.Create a program that asks the user for two numbers and prints the greater
 //of those two. The program should also handle the case in which the two numbers
 //are equal.
 
-//import java.util.Scanner;
-//
-//public class Main {
 //
 //    public static void main(String[] args){
-//
+
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("input first number");
 //        int FNum = input.nextInt();
@@ -102,8 +88,9 @@
 //        } else {
 //            System.out.print( "Second > First");
 //        }
+//     }
+
+   
 //
-//    }
 //
-//
-//}
+}
--- lab2/lab2.java
+++ lab2/lab2.java
@@ -1,8 +1,25 @@
 public class lab2 {
     
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
+    // public static void main(String[] args){
+    //     System.out.println("hello");
+    // }
 
-    
+    // 1.Create a program that asks the user
+    //  for a password. If the password is 
+    //  right, a secret message is shown to 
+    //  the user.
+
+import java.util.Scanner;
+
+public static void main(String[] args){
+    Scanner input = Scanner
+}
+
+ 
+
+
+
+
+
+
 }
