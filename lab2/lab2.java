507f104280e49ffe92dc4bfb916329d74ecc8536
Gemilang-dev <gemilang280505@gmail.com>
2024-10-08 12:47:16 +0200

    Solve lab num 1-3

--- lab2/lab2.java
+++ lab2/lab2.java
@@ -1,20 +1,71 @@
+import java.util.Scanner;
 public class lab2 {
     
-    // public static void main(String[] args){
-    //     System.out.println("hello");
-    // }
-
     // 1.Create a program that asks the user
     //  for a password. If the password is 
     //  right, a secret message is shown to 
     //  the user.
 
-import java.util.Scanner;
 
-public static void main(String[] args){
-    Scanner input = Scanner
-}
 
+// public static void main(String[] args){
+
+//     Scanner input = new Scanner(System.in);
+
+//     String truePassword = "carrot";
+//     boolean userStatus = false;
+
+//     while (userStatus==false){
+
+
+//         System.out.println("Enter the password bro:");
+//         String userInput = input.nextLine();
+
+        
+//         if (userInput.equals(truePassword)){
+//             System.out.print("true jirrr");
+//             System.out.println("Secret message is = geda gedi sigma ");
+//             userStatus = true;
+//         } else {
+//             System.out.print("Wrong password bro");
+    
+//         }
+
+//     }
+
+
+// }
+
+// 2.Create a program that asks the user for three numbers and then prints their sum.
+
+// public static void main(String[] args){
+//     Scanner input = new Scanner(System.in);
+//     int numOfNumbers = 3;
+//     int sum = 0;
+//     while ( numOfNumbers!= 0){
+//         System.out.println("Enter number =");
+//         int userNumber = input.nextInt();
+//         sum += userNumber;
+//         numOfNumbers--;
+//     }
+//     System.out.println("Sum of your numbers = "+ sum);
+// }
+
+// 3. Create a program that reads numbers from the user and prints their sum. The program should stop asking
+//  for numbers when the user enters the number 0. 
+
+// public static void main(String[] args){
+//     Scanner input = new Scanner(System.in);
+//     int sum = 0;
+//     int userNumber = -1;
+    
+//     while(userNumber != 0){
+//         System.out.println("Enter number broooooo =");
+//         userNumber = input.nextInt();
+//         sum += userNumber;
+//     }
+//     System.out.println("Sum of your numbers = "+ sum);
+// }
  
 
 
